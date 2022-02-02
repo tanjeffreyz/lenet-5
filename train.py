8fc7d610f1da9c2f6e1040cecafb580246287f5b
Jeffrey Tan <tanjeffreyz02@gmail.com>
2022-02-02 11:34:48 -0800

    it convergedgit status!

--- train.py
+++ train.py
@@ -1,18 +1,24 @@
 """Training grounds for Py-LeNet-5."""
 
 import torch
-import time
 import os
 import torchvision.transforms as T
 from torchvision.datasets.mnist import MNIST
+from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
+from datetime import datetime
 from tqdm import tqdm
 from models import PyLeNet5
 from matplotlib import pyplot as plt
 
 
-model_dir = 'models/lenet-5'
-weights_dir = os.path.join(model_dir, 'weights')
+writer = SummaryWriter()
+
+model_dir = os.path.join('models', 'lenet-5')
+now = datetime.now()
+date_dir = os.path.join(model_dir, now.strftime('%m_%d_%Y'))
+time_dir = os.path.join(date_dir, now.strftime('%H_%M_%S'))
+weights_dir = os.path.join(time_dir, 'weights')
 if not os.path.isdir(weights_dir):
     os.makedirs(weights_dir)
 
@@ -21,16 +27,12 @@ learning_rate = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 loss_function = torch.nn.CrossEntropyLoss()
 
-train_set = MNIST('data/MNIST',
-                  train=True,
-                  download=True,
+train_set = MNIST('data/MNIST', train=True, download=True,
                   transform=T.Compose([
                       T.Resize((32, 32)),
                       T.ToTensor()]))
 
-test_set = MNIST('data/MNIST',
-                 train=False,
-                 download=True,
+test_set = MNIST('data/MNIST', train=False, download=True,
                  transform=T.Compose([
                      T.Resize((32, 32)),
                      T.ToTensor()]))
@@ -41,10 +43,14 @@ train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=test_batch_size)
 
 # Initialize plots
-fig, axs = plt.subplots(2, 1)
-losses = axs[0]
-losses.set_xlabel('Epoch')
-losses.set_ylabel('Loss')
+axes = plt.figure().add_subplot(111)
+axes.set_autoscale_on(True)
+train_plot, = plt.plot([], [], 'r.-', label='Train loss')
+test_plot, = plt.plot([], [], 'g.-', label='Test loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.yscale('log')
+plt.legend()
 
 
 #########################
@@ -54,43 +60,41 @@ train_epochs = []
 test_epochs = []
 train_losses = []
 test_losses = []
-for epoch in range(1_000):
+for epoch in range(50):
     print(f'[~] Epoch {epoch}: ')
+
     train_loss = 0
-    for image, target in tqdm(train_loader):
+    for images, target in tqdm(train_loader):
         optimizer.zero_grad()
-        output = model.forward(image)
+        output = model.forward(images)
         loss = loss_function(output, target)
         loss.backward()
-
+        optimizer.step()
         train_loss += loss.item() / len(train_loader) * train_batch_size
-
+    writer.add_scalar('Loss/train', train_loss, epoch)
     train_epochs.append(epoch)
     train_losses.append(train_loss)
 
-    if (epoch + 1) % 1 == 0:
+    if epoch % 1 == 0:
         torch.save(model.state_dict(), os.path.join(weights_dir, f'cp_{epoch}'))
-
         print(f' ~  Testing: ')
         test_loss = 0
         with torch.no_grad():
-            for image, target in tqdm(test_loader):
-                output = model.forward(image)
+            for images, target in tqdm(test_loader):
+                output = model.forward(images)
                 loss = loss_function(output, target)
 
                 test_loss += loss.item() / len(test_loader) * train_batch_size
-
+        writer.add_scalar('Loss/test', test_loss, epoch)
         test_epochs.append(epoch)
         test_losses.append(test_loss)
 
-    axs = axs.ravel()
-    losses = axs[0]
-    losses.plot(train_epochs, train_losses, 'r.-', label='Train loss')
-    losses.plot(test_epochs, test_losses, 'g.-', label='Test loss')
-    if epoch == 0:
-        losses.legend()
-    plt.draw()
+    train_plot.set_data(train_epochs, train_losses)
+    test_plot.set_data(test_epochs, test_losses)
+    axes.relim()
+    axes.autoscale_view(True, True, True)
     plt.tight_layout()
-    plt.savefig(os.path.join(model_dir, 'losses.png'))
+    plt.draw()
+    plt.savefig(os.path.join(time_dir, 'losses.png'))
 
 torch.save(model.state_dict(), os.path.join(weights_dir, 'final'))
